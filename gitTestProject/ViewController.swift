4721e541dc1066f8af12129706d00753ff9170af
Berker <berkerertan@hotmail.com>
2020-11-14 00:49:16 +0300

    no message

---
+++ gitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTestProject
+//
+//  Created by Berker ERTAN on 14.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("git")
+    }
+
+
+}
+
